a61320588e2170c6628086ea427ba5a7463f8f94
GiovaniKill <giovanikill@hotmail.com>
2022-03-07 14:35:38 -0400

    Primeiro exercicio - variaveis - scripts.js

---
+++ fundamentos/bloco_04_introducao_javascript/dia_1_javascript-primeiros-passos/scrips.js
@@ -0,0 +1,11 @@
+const myName = "Giovani";
+const birthCity = "Dourados";
+let birthYear = 2022;
+
+birthYear = 2030;
+birthCity = "Campo Grande";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
