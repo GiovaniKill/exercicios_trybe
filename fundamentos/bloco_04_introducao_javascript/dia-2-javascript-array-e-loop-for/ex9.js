3820e5c43cadfd4a15731ebae9159bbead5fe401
GiovaniKill <giovanikill@hotmail.com>
2022-03-08 17:52:53 -0400

    Exercicios 1.8 e 1.9

--- fundamentos/bloco_04_introducao_javascript/dia-2-javascript-array-e-loop-for/ex9.js
+++ fundamentos/bloco_04_introducao_javascript/dia-2-javascript-array-e-loop-for/ex9.js
@@ -1,9 +1,10 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let array = [];
 
-for(let index = 0; index < numbers.length; index+=1){
-    sum += numbers[index];
-    if(index == numbers.length - 1){
-        console.log(sum / numbers.length);
-    }
+for(let index = 1; index <= 25; index++){
+    array.push(index);
+}
+
+
+for(let index = 0; index < array.length; index++){
+    console.log(array[index] / 2);
 }
