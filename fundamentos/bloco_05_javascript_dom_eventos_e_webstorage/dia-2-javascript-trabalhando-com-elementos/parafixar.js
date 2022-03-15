2f1c5b2746893607b0496c18f0adc8ae2a5893f5
GiovaniKill <giovanikill@hotmail.com>
2022-03-15 14:07:33 -0400

    Exercicios para fixar

---
+++ fundamentos/bloco_05_javascript_dom_eventos_e_webstorage/dia-2-javascript-trabalhando-com-elementos/parafixar.js
@@ -0,0 +1,8 @@
+document.getElementById('elementoOndeVoceEsta');
+document.getElementById('elementoOndeVoceEsta').parentNode.style.backgroundColor = 'blue';
+document.getElementById('elementoOndeVoceEsta').firstElementChild.innerText = 'Opa, bão memo?';
+document.getElementById('pai').firstElementChild;
+document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+document.getElementById('elementoOndeVoceEsta').nextSibling;
+document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+document.getElementById('pai').children[2];
