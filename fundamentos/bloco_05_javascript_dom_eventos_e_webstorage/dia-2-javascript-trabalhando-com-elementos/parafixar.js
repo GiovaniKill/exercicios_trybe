8233ff04c5d752db64f83462b5ddbaea3b80f59a
GiovaniKill <giovanikill@hotmail.com>
2022-03-15 14:24:53 -0400

    Exercicios para fixar 2

--- fundamentos/bloco_05_javascript_dom_eventos_e_webstorage/dia-2-javascript-trabalhando-com-elementos/parafixar.js
+++ fundamentos/bloco_05_javascript_dom_eventos_e_webstorage/dia-2-javascript-trabalhando-com-elementos/parafixar.js
@@ -6,3 +6,16 @@ document.getElementById('elementoOndeVoceEsta').previousElementSibling;
 document.getElementById('elementoOndeVoceEsta').nextSibling;
 document.getElementById('elementoOndeVoceEsta').nextElementSibling;
 document.getElementById('pai').children[2];
+
+let irmao = document.createElement('section')
+let pai = document.getElementById('pai')
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
+let filhoDoElementoOndeVoceEsta = document.createElement('section')
+let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho')
+let filhoDoPrimeiroFilhoDoFilho = document.createElement('section')
+
+pai.appendChild(irmao);
+elementoOndeVoceEsta.appendChild(filhoDoElementoOndeVoceEsta);
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+console.log(filhoDoPrimeiroFilhoDoFilho.parentNode.parentNode.nextElementSibling)
