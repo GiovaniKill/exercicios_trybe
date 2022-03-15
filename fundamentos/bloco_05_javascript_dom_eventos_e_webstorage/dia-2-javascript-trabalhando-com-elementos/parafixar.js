bd33af17a245afa5203ab0838c8fa28ece699171
GiovaniKill <giovanikill@hotmail.com>
2022-03-15 14:49:30 -0400

    Exercicio para fixar 3

--- fundamentos/bloco_05_javascript_dom_eventos_e_webstorage/dia-2-javascript-trabalhando-com-elementos/parafixar.js
+++ fundamentos/bloco_05_javascript_dom_eventos_e_webstorage/dia-2-javascript-trabalhando-com-elementos/parafixar.js
@@ -8,14 +8,31 @@ document.getElementById('elementoOndeVoceEsta').nextElementSibling;
 document.getElementById('pai').children[2];
 
 let irmao = document.createElement('section')
+irmao.className = 'irmao'
 let pai = document.getElementById('pai')
 let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
 let filhoDoElementoOndeVoceEsta = document.createElement('section')
+filhoDoElementoOndeVoceEsta.className = 'filhoDoElementoOndeVoceEsta'
 let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho')
 let filhoDoPrimeiroFilhoDoFilho = document.createElement('section')
+filhoDoPrimeiroFilhoDoFilho.className = "filhoDoPrimeiroFilhoDoFilho"
 
 pai.appendChild(irmao);
 elementoOndeVoceEsta.appendChild(filhoDoElementoOndeVoceEsta);
 primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
 
-console.log(filhoDoPrimeiroFilhoDoFilho.parentNode.parentNode.nextElementSibling)
+filhoDoPrimeiroFilhoDoFilho.parentNode.parentNode.nextElementSibling;
+
+let paiDoPai = document.getElementById('paiDoPai')
+let primeiroFilho = document.getElementById('primeiroFilho')
+let segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+let terceiroFilho  = document.getElementById('terceiroFilho')
+let quartoEUltimoFilho = document.getElementById('quartoEUltimoFilho')
+
+pai.removeChild(primeiroFilho)
+elementoOndeVoceEsta.removeChild(segundoEUltimoFilhoDoFilho)
+pai.removeChild(terceiroFilho)
+pai.removeChild(quartoEUltimoFilho)
+pai.removeChild(irmao)
+elementoOndeVoceEsta.removeChild(filhoDoElementoOndeVoceEsta)
+primeiroFilhoDoFilho.removeChild(filhoDoPrimeiroFilhoDoFilho)
